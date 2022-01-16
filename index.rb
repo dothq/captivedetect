b6eb6b7746f520eef7586299f387848ea70aa487
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-01-16 15:15:49 +0000

    ⚡️ Simplify captive portal

--- index.rb
+++
@@ -1,6 +0,0 @@
-Handler = Proc.new do |req, res|
-    res.status = 200
-    res["Content-Type"] = "text/plain"
-    res["Cache-Control"] = "public, max-age=0, must-revalidate"
-    res.body = "Success"
-end
